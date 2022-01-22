9a6a6ed77161affe899f8ad77fb2c4309e3c6c04
JMP <jmproctor14@live.com>
2022-01-22 14:40:56 -0600

    added questions to ask user

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,113 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+
+const questions = () => {
+    return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the title of your project? (Required)',
+                validate: (nameInput) => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        return 'Please enter a title to continue.'
+                    };
+                }
+            },
+            {
+                type: 'input',
+                name: 'username',
+                message: 'What is your Github username? (Required)',
+                validate: (usernameInput) => {
+                    if (usernameInput) {
+                        return true;
+                    } else {
+                        return 'Please provide your Github username to continue.'
+                    };
+                }
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'Email: (Required)',
+                validate: (emailInput) => {
+                    if (emailInput) {
+                        return true;
+                    } else {
+                        return 'Please provide your email address.'
+                    };
+                }
+            },
+            {
+                type: 'input',
+                name: 'Description',
+                message: 'Please add a description of your project.'
+            },
+            {
+                type: 'input',
+                name: 'installation',
+                message: 'Please add step-by-step instructions needed to get the project running for testing. (Required)',
+                validate: (installInput) => {
+                    if (installInput) {
+                        return true;
+                    } else {
+                        return 'Please enter the installation instructions to continue.'
+                    };
+                }
+            },
+            {
+                type: 'input',
+                name: 'usage',
+                message: 'Please provide instructions for use. (Required)',
+                validate: usageInput => {
+                    if (usageInput) {
+                        return true
+                    } else {
+                        console.log('Please enter usage instructions for your project.');
+                        return false
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'tests',
+                message: 'What commands are needed to run tests?'
+            },
+            {
+                type: 'list',
+                name: 'licenseSelection',
+                message: "What license is being used? If you are not using one, please select 'None'.",
+                choices: ['None','Apache 2.0', 'Boost', 'Eclipse', 'GNU', 'IBM', 'ISC', 'MIT', 'Mozilla', 'Open Database License', 'Perl', 'The Hippocratic License'] 
+            },
+            {
+                type: 'input',
+                name: 'contributors',
+                message: 'Who are the contributors?'
+            },
+            {
+                type: 'input',
+                name: 'questions',
+                message: 'Contact info for questions. (Required)',
+                validate: inquiriesInput => {
+                    if (inquiriesInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter contact info for inquiries or questions about your project.');
+                        return false;
+                    }
+                }
+            },
+        ])}
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
 
 // Function call to initialize app
 init();
